20044de53337f7611a6cf9e1a46785ce1db4c86c
job_talk <job-talk@users.noreply.github.com>
2024-08-10 14:20:21 -0500

    Navbar, sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Mark', lastName: 'Tut', email: 'me@mark.com'};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn ? loggedIn.firstName || 'Guest' : 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+
+          />
+
+         <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+         />  
+        </header>
+
+        RECENT TRANSactions
+      </div>  
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
+    </section>
   )
 }
 
